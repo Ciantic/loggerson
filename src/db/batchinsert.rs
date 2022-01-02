4f634b60854540418344f4396c81f95870899ed9
Jari Pennanen <ciantic@oksidi.com>
2022-01-02 02:28:27 +0200

    todo...

--- src/db/batchinsert.rs
+++ src/db/batchinsert.rs
@@ -2,31 +2,32 @@ use std::{collections::HashMap, marker::PhantomData};
 
 use rusqlite::{Connection, Row, ToSql};
 
-// struct DumParams<'a, const N: usize>([&'a dyn ToSql; N]);
-// impl<'a, const N: usize> Sealed for DumParams<'a, N> {}
-
-struct BatchInsertQuery<Key, PFun, Fun, Input, Output, const N: usize>
+struct BatchInsertQuery<Input, Key, FnInputToParams, FnRowToKey, const N: usize>
 where
-    Fun: FnOnce(&Row<'_>) -> rusqlite::Result<(Key, Output)> + Copy,
-    for<'i> PFun: (FnOnce(&'i Input) -> [&'i dyn ToSql; N]) + Copy,
+    FnRowToKey: FnOnce(&Row<'_>) -> rusqlite::Result<Key> + Copy,
+    for<'i> FnInputToParams: (FnOnce(&'i Input) -> [&'i dyn ToSql; N]) + Copy,
 {
     insert_sql: String,
     cache_select_sql: String,
-    binder: Box<Fun>,
-    params_binder: Box<PFun>,
-    cache: HashMap<Input, (Key, Output)>,
+    binder: Box<FnRowToKey>,
+    params_binder: Box<FnInputToParams>,
+    cache: HashMap<Input, Key>,
 }
 
-impl<Key, PFun, Fun, Input, Output, const N: usize>
-    BatchInsertQuery<Key, PFun, Fun, Input, Output, N>
+impl<Input, Key, FnInputToParams, FnRowToKey, const N: usize>
+    BatchInsertQuery<Input, Key, FnInputToParams, FnRowToKey, N>
 where
-    Fun: FnOnce(&Row<'_>) -> rusqlite::Result<(Key, Output)> + Copy,
-    for<'i> PFun: (FnOnce(&'i Input) -> [&'i dyn ToSql; N]) + Copy,
+    FnRowToKey: FnOnce(&Row<'_>) -> rusqlite::Result<Key> + Copy,
+    for<'i> FnInputToParams: (FnOnce(&'i Input) -> [&'i dyn ToSql; N]) + Copy,
     Input: Eq + std::hash::Hash + Clone,
-    Output: Eq + std::hash::Hash + Clone,
     Key: Clone, // Or is Copy better?
 {
-    pub fn new(insert_sql: &str, cache_select_sql: &str, params_binder: PFun, binder: Fun) -> Self {
+    pub fn new(
+        insert_sql: &str,
+        cache_select_sql: &str,
+        params_binder: FnInputToParams,
+        binder: FnRowToKey,
+    ) -> Self {
         BatchInsertQuery {
             insert_sql: insert_sql.to_owned(),
             cache_select_sql: cache_select_sql.to_owned(),
@@ -36,8 +37,15 @@ where
         }
     }
 
-    pub fn insert(&mut self, con: &mut Connection, entries: &Vec<&Input>) -> Vec<(Key, Output)> {
+    pub fn insert(&mut self, con: &mut Connection, entries: &Vec<&Input>) -> Vec<Key> {
         // TODO: Proper Result return type
+
+        // TODO: Run Cache SQL, get [Key, Input]... values, then store these to
+        // `cache` before insert
+
+        // TODO: What if only part of the input value is unique? Maybe I need
+        // also a FnOnce(Input) -> CacheKey? (Key is Insertion key, not cache
+        // key)
 
         let tx = con.transaction().unwrap();
         let results = {
@@ -60,10 +68,9 @@ where
                     }
                     let mut rows = insertor.raw_query();
                     let row = rows.next().unwrap().unwrap();
-                    let (key, output) = self.binder.as_ref()(row).unwrap();
-                    self.cache
-                        .insert(i.to_owned().to_owned(), (key.to_owned(), output.to_owned()));
-                    (key, output)
+                    let key = self.binder.as_ref()(row).unwrap();
+                    self.cache.insert(i.to_owned().to_owned(), key.to_owned());
+                    key
                 })
                 .collect()
         };
@@ -102,7 +109,7 @@ mod tests {
             "INSERT INTO people (name, address) VALUES (?, ?) RETURNING id",
             "Doo",
             |input: &Person| [&input.name, &input.address],
-            |row| Ok((row.get_unwrap::<_, i32>(0), ())),
+            |row| Ok(row.get_unwrap::<_, i32>(0)),
         );
 
         biq.insert(
